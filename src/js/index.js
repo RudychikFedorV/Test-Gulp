b3edd0d279c8bd40f9d9dc152ef2551414f55b95
RudychikFedorV <rudychik.fedor@gmail.com>
2024-03-29 21:43:48 +0200

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -12,24 +12,4 @@ mobileNav();
 
 
 
-import Swiper from "swiper/bundle";
-import "swiper/css/bundle";
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  slidesPerView: 5.5,
-  parallax: true,
-  speed: 1000,
-  keyboard: {
-	enabled: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+
--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -12,3 +12,25 @@ function mobileNav() {
 }
 
 export default mobileNav;
+
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 5,
+  parallax: true,
+  speed: 1000,
+  keyboard: {
+	enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
