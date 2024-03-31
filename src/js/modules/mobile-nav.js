324512551272f6db651bac88055b1c96327cb99e
RudychikFedorV <rudychik.fedor@gmail.com>
2024-03-31 14:08:12 +0300

    footer

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -17,7 +17,7 @@ import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 5,
+  slidesPerView: 5.5,
   parallax: true,
   speed: 1000,
   keyboard: {
